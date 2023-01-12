fcb00993a38f4789567f4120f9624f823dc76c4b
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-01-12 20:58:38 +0200

    + latest api structure

--- src/api/elements/example.rs
+++
@@ -1,9 +0,0 @@
-use cooplan_amqp_api::api::element::Element;
-use cooplan_amqp_api::config::api::config::Config;
-use crate::logic::logic_request::LogicRequest;
-use cooplan_amqp_api::error::{Error};
-
-
-pub fn get(config: &Config) -> Result<Element<LogicRequest>, Error> {
-    todo!()
-}
