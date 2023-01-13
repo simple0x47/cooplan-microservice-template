3b2ff840d975600880121987883e7a6a4f86b6e6
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-01-13 13:17:14 +0200

    + use latest 'cooplan-amqp-api'

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,15 +1,21 @@
 use cooplan_amqp_api::config::openid_connect_config::OpenIdConnectConfig;
-use serde::{Deserialize, Serialize};
+use cooplan_lapin_wrapper::config::amqp_connect_config::AmqpConnectConfig;
+use cooplan_state_tracker::state_tracking_config::StateTrackingConfig;
+use serde::Deserialize;
 
 use crate::error::{Error, ErrorKind};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct Config {
     pub openid_connect: OpenIdConnectConfig,
     pub logic_request_dispatch_instances: u16,
-    pub logic_requests_bound: usize,
+    pub logic_requests_boundary: usize,
     pub storage_request_dispatch_instances: u16,
-    pub storage_requests_bound: usize,
+    pub storage_requests_boundary: usize,
+    pub output_channel_boundary: usize,
+    pub amqp_api_connect: AmqpConnectConfig,
+    pub state_tracking_config: StateTrackingConfig,
+    pub state_tracking_channel_boundary: usize,
 }
 
 pub async fn try_read_config(config_file: &str) -> Result<Config, Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use crate::api::input::elements::example::get;
+use crate::config::config::Config;
 use crate::logic::logic_request::LogicRequest;
 use crate::logic::storage_request::StorageRequest;
 use cooplan_amqp_api::api::initialization_package::InitializationPackage;
+use cooplan_lapin_wrapper::config::api::Api;
+use serde_json::Value;
 use std::io::{Error, ErrorKind};
 use std::time::Duration;
 
@@ -22,38 +26,33 @@ async fn main() -> Result<(), Error> {
         }
     }
 
-    let api_file = match std::env::args().nth(1) {
-        Some(api_file) => api_file,
-        None => {
-            return Err(Error::new(ErrorKind::InvalidInput, "no api file provided"));
-        }
-    };
+    let api = get_api().await?;
 
-    let config_file = match std::env::args().nth(2) {
-        Some(config_file) => config_file,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                "no config file provided",
-            ))
-        }
-    };
+    let api_config = get_api_config().await?;
 
-    let config = match config::config::try_read_config(&config_file).await {
-        Ok(config) => config,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::Other,
-                format!("failed to read config: {}", error),
-            ));
-        }
-    };
+    let config = get_config().await?;
 
     let (logic_request_sender, logic_request_receiver) =
-        async_channel::bounded::<LogicRequest>(config.logic_requests_bound);
+        async_channel::bounded::<LogicRequest>(config.logic_requests_boundary);
 
-    let api_package =
-        InitializationPackage::new(logic_request_sender, Box::new(api::registration::register));
+    let (output_sender, output_receiver) =
+        tokio::sync::mpsc::channel::<(String, Value)>(config.output_channel_boundary);
+
+    let state_tracker_client = cooplan_state_tracker::state_tracker_client::build(
+        config.state_tracking_config,
+        config.state_tracking_channel_boundary,
+    )
+    .await;
+
+    let api_package = InitializationPackage::new(
+        logic_request_sender,
+        Box::new(api::input::registration::register),
+        output_receiver,
+        Box::new(api::output::registration::register),
+        api,
+        api_config,
+        state_tracker_client,
+    );
 
     match cooplan_amqp_api::api::init::initialize(api_package).await {
         Ok(()) => (),
@@ -66,7 +65,7 @@ async fn main() -> Result<(), Error> {
     }
 
     let (storage_request_sender, storage_request_receiver) =
-        async_channel::bounded::<StorageRequest>(config.storage_requests_bound);
+        async_channel::bounded::<StorageRequest>(config.storage_requests_boundary);
 
     match logic::init::initialize(
         config.logic_request_dispatch_instances,
@@ -102,4 +101,74 @@ async fn main() -> Result<(), Error> {
     std::thread::sleep(Duration::MAX);
 
     Ok(())
+}
+
+async fn get_api() -> Result<Api, Error> {
+    let api_file = match std::env::args().nth(1) {
+        Some(api_file) => api_file,
+        None => {
+            return Err(Error::new(ErrorKind::InvalidInput, "no api file provided"));
+        }
+    };
+
+    let api = match cooplan_lapin_wrapper::config::api::try_get(api_file.as_str()).await {
+        Ok(api) => api,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("failed to read api: {}", error),
+            ))
+        }
+    };
+
+    Ok(api)
+}
+
+async fn get_api_config() -> Result<cooplan_amqp_api::config::config::Config, Error> {
+    let api_config_file = match std::env::args().nth(2) {
+        Some(config_file) => config_file,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "no config file provided",
+            ))
+        }
+    };
+
+    let api_config = match cooplan_amqp_api::config::config::try_read_config(&api_config_file).await
+    {
+        Ok(api_config) => api_config,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("failed to read config: {}", error),
+            ));
+        }
+    };
+
+    Ok(api_config)
+}
+
+async fn get_config() -> Result<Config, Error> {
+    let config_file = match std::env::args().nth(2) {
+        Some(config_file) => config_file,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "no config file provided",
+            ))
+        }
+    };
+
+    let config = match config::config::try_read_config(&config_file).await {
+        Ok(config) => config,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("failed to read config: {}", error),
+            ));
+        }
+    };
+
+    Ok(config)
 }
